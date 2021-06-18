799c240de71d55b406ce22a8990926ab675c4ea9
AhmadAbdElHakim <ahmad-abdelhakim@hotmail.com>
2021-06-18 11:20:14 +0200

    Sample images and opaque watermark to be added
    Comments added.

--- RemoveLogoAndTransparent.py
+++ RemoveLogoAndTransparent.py
@@ -4,18 +4,21 @@ import numpy as np
 imagePath = '' 
 
 def removeLogo(filename, outputfilename):
+    # Read Image
     img = cv2.imread(filename)
+    # Resize
     img = cv2.resize(img,(400,500))
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    # Thresholding
     ret, gray = cv2.threshold(gray,127,255,0)
     gray2 = gray.copy()
-
+    # Find Contours
     contours, hier = cv2.findContours(gray,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     for cnt in contours:
-        if 200<cv2.contourArea(cnt)<5000:
+        if 200<cv2.contourArea(cnt) < 5000:
             (x,y,w,h) = cv2.boundingRect(cnt)
-            cv2.rectangle(gray2,(x,y),(x+w,y+h),0,-1)
-
+            cv2.rectangle(gray2,(x, y),(x + w, y + h), 0, -1)
+    # Write Image
     cv2.imwrite(outputfilename,gray2)
 
 removeLogo(imagePath, 'out.png')
