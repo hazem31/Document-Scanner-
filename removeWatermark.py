e0469d1e9e38fdfb04f1344b00f77fbd553c8615
ahmedalaa11 <47926165+ahmedalaa11@users.noreply.github.com>
2021-06-18 14:18:22 +0200

    Add files via upload

---
+++ removeWatermark.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+
+imagePath = '' 
+
+def removeWatermark(filename, outputfilename):
+    # Load the image
+    img = cv2.imread(filename)
+
+    # Convert the image to grayscale
+    gr = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Make a copy of the grayscale image
+    bg = gr.copy()
+
+    # Apply morphological transformations
+    for i in range(5):
+        kernel2 = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,
+                                            (2 * i + 1, 2 * i + 1))
+        bg = cv2.morphologyEx(bg, cv2.MORPH_CLOSE, kernel2)
+        bg = cv2.morphologyEx(bg, cv2.MORPH_OPEN, kernel2)
+
+    # Subtract the grayscale image from its processed copy
+    dif = cv2.subtract(bg, gr)
+
+    # Apply thresholding
+    bw = cv2.threshold(dif, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    dark = cv2.threshold(bg, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    # Extract pixels in the dark region
+    darkpix = gr[np.where(dark > 0)]
+
+    # Threshold the dark region to get the darker pixels inside it
+    darkpix = cv2.threshold(darkpix, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    # Paste the extracted darker pixels in the watermark region
+    bw[np.where(dark > 0)] = darkpix.T
+
+    cv2.imwrite(outputfilename, bw)
+
+removeWatermark(imagePath, 'out.jpg')
